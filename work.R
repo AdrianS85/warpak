297ee4327df72e47ba8626c4bc997a2129fe8cd4
AdrianS85 <adrianstankiewicz85@gmail.com>
2024-06-01 10:42:57 +0200

    1

---
+++ work.R
@@ -0,0 +1,32 @@
+renv::activate()
+
+usethis::create_package(path = ".")
+
+
+
+
+#' List available datasets from a source
+#'
+#' @param source String. Which source to query
+#'
+#' @return Character vector
+#' @export
+#'
+#' @examples
+list_datasets <- function(
+    source, 
+  
+    )
+{
+  "https://github.com/scarnecchia/oryx_data/tree/main"
+  
+  
+}
+
+get_oryx_datasets <- function(
+    )
+{
+  
+  # https://raw.githubusercontent.com/scarnecchia/oryx_data/main/event_destroyed.csv
+  
+}
